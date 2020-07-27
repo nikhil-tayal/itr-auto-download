05f49b23e1c68c9541ecebb7589cfcb155f35bd6
tayalnikhil <nikhil@credochain.com>
2020-07-27 12:00:08 +0530

    completd

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const express = require("express");
+const cors = require("cors");
 const app = express();
-require("./src/puppeteer");
+const { puppeteerOTPTrigger, puppeteerOTPVerification } = require("./src/puppeteer");
+app.use(express.json());
+app.use(cors());
 app.get("/", (req, res) => {
   res.send("Hi");
 });
@@ -10,6 +13,9 @@ app.listen(PORT, () => {
   console.log(`Listening on Port${PORT}`);
 });
 
-app.post("/otp-verify", (req, res) => {
-  testFunction(req.body.otp);
+app.post("/username-password", (req, res) => {
+  puppeteerOTPTrigger(req.body);
+});
+app.post("/otp-verification", (req, res) => {
+  puppeteerOTPVerification(req.body);
 });
--- src/puppeteer.js
+++ src/puppeteer.js
@@ -3,30 +3,69 @@ const $ = require("cheerio");
 // const jsdom = require("jsdom");
 // const { JSDOM } = jsdom;
 
-(async () => {
+const puppeteerOTPTrigger = async ({ username, password }) => {
+  this.username = username;
+  this.password = password;
   const browser = await puppeteer.launch({ headless: false });
   const page = await browser.newPage();
   await page.goto("https://www1.incometaxindiaefiling.gov.in/e-FilingGS/Registration/RegistrationHome.html?lang=eng");
   await Promise.all([page.waitForNavigation(), page.click(".personalDetails>div>a")]);
-  await page.$eval("#Login_userName", (el) => (el.value = "BDIPT4002P"));
-  await page.$eval("#Login_password", (el) => (el.value = "Tayal@741852@"));
+  await page.$eval(
+    "#Login_userName",
+    (el, username) => {
+      return (el.value = username);
+    },
+    username
+  );
+  await page.$eval("#Login_password", (el, password) => (el.value = password), password);
   await page.evaluate(() => {
     document.querySelector("#otpLogin").checked = true;
   });
-//   await Promise.all([page.waitForNavigation(), page.click("#button1")]);
-//   await Promise.all([page.waitForNavigation(), page.click(".btnOrange")]);
-  
+  this.page = page;
+  // await page.screenshot({ path: "example.png" });
+  await Promise.all([page.waitForNavigation(), page.click("#button1")]);
+  await Promise.all([page.waitForNavigation(), page.click(".btnOrange")]); // it will generate the otp
+  // let otp = await getOtpInput();
   //   await browser.close();
-})();
+};
 
-// (async () => {
-//   const browser = await puppeteer.launch();
-//   const page = await browser.newPage();
-//   await page.goto("https://www.incometaxindiaefiling.gov.in/home");
-//   let html = await page.evaluate(() => document.body.innerHTML);
-//   await page.click(".ui-button.ui-widget.ui-state-default.ui-corner-all.ui-button-icon-only.ui-dialog-titlebar-close");
-//   await Promise.all([page.waitForNavigation(),  page.click(".sec_btn")]);
-//   await page.screenshot({ path: "example.png" });
+const puppeteerOTPVerification = async ({ otp }) => {
+  let page = this.page;
+  await page.$eval("#OTPLoginValidate_otp", (el, otp) => (el.value = otp), otp);
+  await Promise.all([page.waitForNavigation(), page.click(".btnOrange")]);
+  if ((await page.$(".btnOrange")) !== null) {
+    await Promise.all([page.waitForNavigation(), page.click(".btnOrange")]);
+  }
+  const [myAccountButton] = await page.$x("//span[contains(., 'My Account')]");
+  if (myAccountButton) {
+    await myAccountButton.hover();
+  }
 
-//   await browser.close();
-// })();
+  const [button] = await page.$x("//a[contains(., 'Download Pre-filled XML')]");
+  if (button) {
+    // await button.click();
+    await Promise.all([page.waitForNavigation(), button.click()]);
+  }
+  await page.select('select[name="asYear"]', "2019"); // ITR YEAR selected 
+  await page.select('select[name="formId"]', "ITR-1"); // ITR TYPE selected 
+
+  
+  await Promise.all([page.waitForNavigation(), page.click("#continueButton")]);  // Continue button to open POPUP
+
+  await page.click("#UpdateContactDtls_0");  // Download XML 
+  // await Promise.all([page.waitForNavigation(), page.click("#UpdateContactDtls_0")]);
+
+  await page.waitFor(500);
+  await page.click("#prefillConcentFlag"); // Check Box click
+  let selector = 'input[name="CntinueBtn"]';
+  await page.evaluate((selector) => document.querySelector(selector).click(), selector); 
+
+
+
+  await page.screenshot({ path: "example.png" });
+};
+
+module.exports = {
+  puppeteerOTPTrigger,
+  puppeteerOTPVerification,
+};
